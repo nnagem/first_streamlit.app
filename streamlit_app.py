0833a73facd72a6bf7c7d621de6b8d9eac5bc5c1
nnagem <nnagem@users.noreply.github.com>
2023-09-27 10:33:54 -0500

    Update streamlit_app.py
    Changed the name of the diner and pre-selected fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -14,7 +14,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Pick list to allow users to select fruits to go into their smoothie
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #Displays table with fruits on the page
 streamlit.dataframe(my_fruit_list)
