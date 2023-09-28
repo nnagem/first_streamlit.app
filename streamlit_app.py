784c3ef066bbdcee0be341decd0ec9ad5f1eb384
nnagem <nnagem@users.noreply.github.com>
2023-09-28 06:48:24 -0500

    Update streamlit_app.py
    Updated the normalised data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 #Displays fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+# normalizes the json version of the response received 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#displays the normalize response in a table
+streamlit.dataframe(fruityvice_normalized)
